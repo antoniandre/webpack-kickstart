51b2bf9fd933971285786c2861af528f98828663
Antoni André <antoniandre.web@gmail.com>
2018-01-13 23:19:44 +1100

    Cleanup.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import './style.css';
-import Icon from './en-flag.png';
 
 function component() {
     var element = document.createElement('h1');
     element.innerHTML = "Hello World! ";
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
 
     return element;
 }
