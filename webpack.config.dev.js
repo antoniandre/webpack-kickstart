f9e4bcfe83e94207ef36254f6b96e0d3432247c4
Antoni André <antoniandre.web@gmail.com>
2018-01-14 00:14:24 +1100

    Add source map, clean dist folder on build and code cleanup.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,32 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'app': './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({ title: 'Webpack Kickstart' })
+    ],
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+        ]
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            'file-loader'
-          ]
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            'file-loader'
-          ]
-        }
-      ]
-    }
 };
